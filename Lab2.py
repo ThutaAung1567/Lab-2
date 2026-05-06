c6331230d3260c15b0b085e4be6400f6db40eb74
Thuta Aung <thuta16.25@ichat.sp.edu.sg>
2026-05-06 11:40:51 +0800

    lab2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,13 @@
-print("ET0735 (DevOps for AIoT) - Lab2 - Introduction to Python")
+def display_main_menu():
+    print()
+def get_user_input(a,b):
+    a=int(input("Enter your first number"))
+    b=int(input("Enter your second number"))
+def calc_average():
+    print("calc_average")
+def find_min_max(min_temp,max_temp):
+    return [min_temp,max_temp]
+def sort_temperature():
+    return
+def calc_median_temperature():
+    return
