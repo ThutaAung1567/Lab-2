834ebb3940007ae726efddc1f291d4b314f6ba9a
Thuta Aung <thuta16.25@ichat.sp.edu.sg>
2026-05-13 06:41:29 +0800

    Update return values

--- bmi.py
+++ bmi.py
@@ -4,16 +4,11 @@ def calculate_bmi(height, weight):
     bmi= weight /(height**2)
     print("BMI="+str(bmi))
     if bmi<18.5:
-        print(underweight())
+        print(-1)
     elif bmi<=25.0:
-        print(Normalweight())
+        print(0)
     else:
-        print(Overweight())
-def underweight():
-    return ("-1")
-def Normalweight():
-    return("0")
-def Overweight():
-    return ("1")
+        print(1)
+
 
 calculate_bmi(weight=57,height=1.73)
